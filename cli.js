f665d5d83482e095daca440ab186b13d5b8e1f98
Gurjeet Singh Virdee <gurjeetsinghvirdee@users.noreply.github.com>
2024-05-20 16:47:44 +0530

    Update CLI usage instructions for better clarity and consistency

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const createBookmarksHtml = require('./index');
+const [,, inputFile, outputFile] = process.argv;
+
+if (!inputFile || !outputFile) {
+    console.error('Usage: txt-to-bookmark <inputFile> <outputFile>');
+    process.exit(1);
+}
+
+createBookmarksHtml(inputFile, outputFile);
+console.log(`Bookmarks have been generated and saved to ${outputFile}`);
+
